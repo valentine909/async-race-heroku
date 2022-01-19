a4bbf2dafe2c8916db2d674b39d2eeb8eab08737
valentine909 <valentine.nazarov@gmail.com>
2022-01-19 16:55:14 +0300

    feat: proper components initialization

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,19 +1,14 @@
 import './style.css';
 import Garage from "./views/Garage";
 import GarageManager from "./models/GarageManager";
-import {AllCars} from "./types/types";
 import Controller from "./controller/Controller";
 
 (async ()=> {
   const root = document.querySelector('#app') as HTMLElement;
   const garageManager = new GarageManager();
-  const allCars =  await garageManager.getCars(1) as AllCars;
   const garage = new Garage();
   const controller = new Controller(root, garageManager, garage);
-  garage.updateRaceTrack(await allCars.cars);
-  garage.updateCarsNumber(allCars.totalCars);
-  garage.updatePageNumber('1');
-  root.append(garage.render());
+  await controller.init();
 })();
 
 // let offset = 0;
